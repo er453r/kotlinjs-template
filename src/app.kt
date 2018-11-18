8febe72d1f1f82ba04f50faa1ad3fff03a663608
er453r <er453r@gmail.com>
2018-11-18 23:53:09 +0100

    jquery module works

--- src/app.kt
+++ src/app.kt
@@ -1,14 +1,27 @@
+import org.w3c.dom.HTMLElement
 import org.w3c.dom.HTMLHeadingElement
 import kotlin.browser.document
+
+@JsModule("jquery")
+external class JQuery() {
+    constructor(selector:String)
+    constructor(element: HTMLElement)
+    fun hide(number:Int):JQuery
+}
 
 fun main(args: Array<String>) {
     println("hello derp!")
 
     document.addEventListener("DOMContentLoaded", {
-        val header = document.createElement("h1") as HTMLHeadingElement
+        val header = document.createElement("h2") as HTMLHeadingElement
 
         header.innerHTML = "LOLOLO"
 
         document.body?.appendChild(header)
+
+        JQuery(header).hide(5000)
+
+        JQuery("h1").hide(5000)
+
     })
 }
